b3a0ea8355453687f17adf70a3e697ba71ddf904
Roman Paradeev <sameoldmadness@gmail.com>
2014-11-29 01:29:56 +0500

    adaptive tables

--- js/app.js
+++ js/app.js
@@ -18,6 +18,41 @@ $.get('index.md').then(function (data) {
       return '<span class="rating">' + r + '</span>';
     }));
   });
+
+  $('table').each(function () {
+    var $table = $(this);
+    var $adaptiveTable = $('<table>').addClass('adaptive');
+
+    $table.find('tr').each(function (i) {
+      var $tr = $(this);
+      var stack = [];
+      var $adaptiveTr = $('<tr>');
+      var $adaptiveTd;
+      var tag = i === 0 ? 'th' : 'td';
+
+      $tr.find('td, th').each(function (j) {
+        var $td = $(this);
+
+        if (j < 2) {
+          $adaptiveTd = $('<' + tag + '>');
+          $adaptiveTd.html($td.html());
+          $adaptiveTd.appendTo($adaptiveTr);
+        } else {
+          stack.push($td.html());
+        }
+      });
+
+      if (stack.length) {
+        $adaptiveTd = $('<' + tag + '>');
+        $adaptiveTd.html(stack.join(i === 0 ? ', ' : '<br>'));
+        $adaptiveTd.appendTo($adaptiveTr);
+      }
+
+      $adaptiveTr.appendTo($adaptiveTable);
+    });
+
+    $adaptiveTable.insertAfter($table);
+  });
 });
 
 }());
